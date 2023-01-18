7924bc828ab693b168affceb501c41ee276ffc8a
narinn-star <skfls2618@naver.com>
2023-01-18 17:56:42 +0900

    (#6) ListItem StyleSheet 수정

--- src/screens/SettingScreen.tsx
+++ src/screens/SettingScreen.tsx
@@ -1,43 +1,61 @@
 import {Text} from '@rneui/base';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 import {ListItem} from '@rneui/themed';
 import Icon from 'react-native-vector-icons/Feather';
 
 export default function SettingScreen() {
   return (
-    <SafeAreaView style={{alignItems: 'center', justifyContent: 'center'}}>
+    <SafeAreaView style={styles.Container}>
       {/*금융 관련 */}
-      <ListItem style={{width: '100%'}}>
-        <ListItem.Title style={{fontSize: 20, fontWeight: 'bold'}}>
-          금융
-        </ListItem.Title>
+      <ListItem>
+        <ListItem.Title style={styles.title}>금융</ListItem.Title>
       </ListItem>
-      <ListItem style={{borderColor: 'black', borderBottomWidth: 1}}>
+      <ListItem style={styles.listContainer}>
         <Icon name="user" size={40} color="#fce085" />
-        <ListItem.Title style={{width: '75%'}}>
-          <Text style={{fontSize: 20}}>카드(계좌) 등록</Text>
+        <ListItem.Title style={styles.contents}>
+          <Text style={styles.text}>카드(계좌) 등록</Text>
         </ListItem.Title>
         <ListItem.Chevron size={40} />
       </ListItem>
       {/*회원 관련 */}
-      <ListItem style={{width: '100%'}}>
-        <ListItem.Title style={{fontSize: 20, fontWeight: 'bold'}}>
-          개인
-        </ListItem.Title>
+      <ListItem>
+        <ListItem.Title style={styles.title}>개인</ListItem.Title>
       </ListItem>
-      <ListItem style={{borderColor: 'black', borderBottomWidth: 1}}>
+      <ListItem style={styles.listContainer}>
         <Icon name="user" size={40} color="#fce085" />
-        <ListItem.Title style={{width: '75%'}}>
-          <Text style={{fontSize: 20}}>회원 정보</Text>
+        <ListItem.Title style={styles.contents}>
+          <Text style={styles.text}>회원 정보</Text>
         </ListItem.Title>
         <ListItem.Chevron size={40} />
       </ListItem>
       <ListItem>
         <Icon name="user" size={40} color="#fce085" />
-        <ListItem.Title style={{width: '90%'}}>
-          <Text style={{fontSize: 20}}>회원 탈퇴</Text>
+        <ListItem.Title style={styles.contents}>
+          <Text style={styles.text}>회원 탈퇴</Text>
         </ListItem.Title>
       </ListItem>
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  Container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  listContainer: {
+    borderColor: 'black',
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    width: '100%',
+  },
+  contents: {
+    width: '75%',
+  },
+  text: {
+    fontSize: 20,
+  },
+});
