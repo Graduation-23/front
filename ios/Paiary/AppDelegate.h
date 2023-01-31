a2883553f02d3d82a2fde6e62e6ce1fcf6ed3d4d
byungjin <byungjin.dev@gmail.com>
2023-01-31 18:23:48 +0900

    feat: Add Deeplink Linking on Navigator(#29)

--- ios/Paiary/AppDelegate.h
+++ ios/Paiary/AppDelegate.h
@@ -1,8 +1,17 @@
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
+#import <React/RCTLinkingManager.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 
 @end
+
+// Add this above `@end`:
+- (BOOL)application:(UIApplication *)application
+   openURL:(NSURL *)url
+   options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  return [RCTLinkingManager application:application openURL:url options:options];
+}
