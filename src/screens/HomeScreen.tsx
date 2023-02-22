ddac2d47faf7a4693ccb9da0747f54b482fbe08a
narinn-star <skfls2618@naver.com>
2023-02-22 20:11:12 +0900

    feat : Capture Completegit add . (#65)

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,75 +5,35 @@ import {useRecoilValue} from 'recoil';
 import userAtom from '../atom/userAtom';
 import {AppText} from '../components/AppText';
 import GrowingPlant from '../features/Home/GrowingPlant';
-import ViewShot, {captureRef} from 'react-native-view-shot';
+import ViewShot from 'react-native-view-shot';
 import {Button} from '@rneui/themed';
 import CameraRoll from '@react-native-community/cameraroll';
+import Share from 'react-native-share';
 
 export default function HomeScreen() {
   const user = useRecoilValue(userAtom);
-  const ref = useRef<any>(null);
-
-  // const onShare = async (uri: any) => {
-  //   const result = await Share.share({
-  //     url: Platform.OS === 'ios' ? `file://${uri}` : uri,
-  //   });
-  // };
+  const captureRef = useRef<any>(null);
 
   const onCapture = () => {
-    if (ref.current !== undefined) {
-      ref.current.capture().then((uri: any) => {
-        console.log('do something with', uri);
-        getPhotoUri();
+    if (captureRef.current !== undefined) {
+      captureRef.current?.capture().then((uri: any) => {
+        console.log('캡쳐하고싶다.', uri);
+        onShare(uri);
       });
     }
-    captureRef(ref, {format: 'jpg', quality: 0.8, result: 'base64'}).then(
-      () => console.log('저장됐다'),
-      error => console.error(error),
-    );
   };
 
-  const getPhotoUri = async (): Promise<any> => {
-    const uri = await ref.current.capture();
-    console.log('Image saved to : ', uri);
-
-    try {
-      if (uri) {
-        const result = CameraRoll.save(uri, {type: 'photo'}).then(() =>
-          console.log('dd'),
-        );
-      }
-    } catch (error) {
-      console.log('ERROR!!', error);
-    }
-  };
-
-  const hasAndroidPermission = async () => {
-    const permission = PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE;
-
-    const hasPermission = await PermissionsAndroid.check(permission);
-    if (hasPermission) {
-      return true;
-    }
-
-    const status = await PermissionsAndroid.request(permission);
-    return status === 'granted';
-  };
-
-  const onSave = async () => {
-    if (Platform.OS === 'android' && !(await hasAndroidPermission())) {
-      console.log('갤러리 접근 권한 없음');
-      return;
-    }
-
-    const uri = await getPhotoUri();
-    const result = CameraRoll.save(uri, {type: 'photo'});
-    console.log('result : ', result);
+  const onShare = async (uri: any) => {
+    const res = Share.open({
+      url: Platform.OS === 'ios' ? `file://${uri}` : uri,
+    });
+    console.log('저장 성공!,', res);
   };
 
   return (
     <ViewShot
-      ref={ref}
-      options={{fileName: 'Your-File-Name', format: 'jpg', quality: 0.9}}>
+      ref={captureRef}
+      options={{fileName: 'Capture-File', format: 'jpg', quality: 0.9}}>
       <SafeAreaView style={styles.Container}>
         <AppText.Title family="round-a">
           {user && user.nickname}님이 키우고 있는 식물
@@ -83,7 +43,7 @@ export default function HomeScreen() {
           <GrowingPlant kind="flower" level={2} type="marigold" />
         </View>
         <Button title="캡쳐" onPress={onCapture} />
-        <Button title="공유" onPress={onSave} />
+        {/* <Button title="공유" onPress={onSave} /> */}
       </SafeAreaView>
     </ViewShot>
   );
@@ -94,6 +54,7 @@ const styles = StyleSheet.create({
     height: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#f2f2f2',
   },
   PlantContainer: {
     flexDirection: 'row',
