7b13f745442605252aec79d19adf2ad46034ee75
narinn-star <skfls2618@naver.com>
2023-02-25 18:01:51 +0900

    feat : Goal_Weekly back 수정중 (#89)

--- src/features/Home/Goal/GoalRegDialog.tsx
+++ src/features/Home/Goal/GoalRegDialog.tsx
@@ -2,7 +2,8 @@ import {AppText} from '@/components/AppText';
 import {Dialog, Input} from '@rneui/themed';
 import {View} from 'react-native';
 import {useState} from 'react';
-import {useRequestMonthGoal} from '@/query/goal';
+import {useRequestMonthGoal, useRequestWeekGoal} from '@/query/goal';
+import fetchMonthGoal from '@/api/goal/fetchMonthGoal';
 
 export type GoalRegDialogProps = {
   visible: boolean;
@@ -17,13 +18,20 @@ export default function GoalRegDialog({
 }: GoalRegDialogProps) {
   const [name, setName] = useState('');
   const [amount, setAmount] = useState('');
-  const {mutateAsync: requestGoal} = useRequestMonthGoal();
+  const {mutateAsync: requestMonthGoal} = useRequestMonthGoal();
+  const {mutateAsync: requestWeekGoal} = useRequestWeekGoal();
 
   const handleGoal = () => {
     select === '월간'
-      ? requestGoal({name: name, amount: parseInt(amount)})
+      ? requestMonthGoal({name: name, amount: parseInt(amount), weekIds: []})
       : //console.log(name, typeof parseInt(amount))
-        console.log('주간');
+        fetchMonthGoal().then(res =>
+          requestWeekGoal({
+            id: res[0].id,
+            name: name,
+            amount: parseInt(amount),
+          }),
+        );
   };
 
   const onChangeName = (text: string) => {
