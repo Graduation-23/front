55067b92f7f4a0528730a6dba58a3b2e9b17f734
byungjin <byungjin.dev@gmail.com>
2023-02-25 15:27:36 +0900

    feat: Add LinkPayment UI

--- src/components/ExternalLinkButton.tsx
+++ src/components/ExternalLinkButton.tsx
@@ -1,7 +1,5 @@
 import {ButtonProps, Button} from '@rneui/base';
-// import {Linking} from 'react-native';
 import InAppBrowser from 'react-native-inappbrowser-reborn';
-import {StyleSheet} from 'react-native';
 
 interface ExternalLinkButtonProps extends ButtonProps {
   url: string;
@@ -14,20 +12,8 @@ const ExternalLinkButton = ({url, ...props}: ExternalLinkButtonProps) => {
       onPress={() => {
         InAppBrowser.open(url).catch(err => console.log(err));
       }}
-      buttonStyle={styles.Btn}
     />
   );
 };
-
-const styles = StyleSheet.create({
-  Btn: {
-    backgroundColor: '#bbdefb',
-    height: 50,
-    borderRadius: 10,
-    borderColor: 'black',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-});
 
 export default ExternalLinkButton;
--- src/screens/LinkPaymentScreen.tsx
+++ src/screens/LinkPaymentScreen.tsx
@@ -1,15 +1,78 @@
-import {Text} from '@rneui/themed';
-import {View} from 'react-native';
+import {AppText} from '@/components/AppText';
+import ExternalLinkButton from '@/components/ExternalLinkButton';
+import {Button, Divider} from '@rneui/base';
+import {StyleSheet, View} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
+import {useRecoilValue} from 'recoil';
+import userAtom from '@atom/userAtom';
 
 const LinkPaymentScreen = () => {
+  const user = useRecoilValue(userAtom);
+
   return (
-    <SafeAreaView>
-      <View>
-        <Text>LinkPaymentScreen 입니다.</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.content}>
+        <AppText style={styles.title}>Paiary with</AppText>
+        <AppText style={styles.subtitle}>금융결제원</AppText>
+        <AppText style={styles.typo}>간편하게 지출 내역 가져오기</AppText>
+        <ExternalLinkButton
+          buttonStyle={styles.linkButton}
+          url={`http://account-diary.kro.kr:8080/api/auth/openbank/uri?user-id=${user?.id}`}>
+          연동하기
+        </ExternalLinkButton>
+        {/* <Button buttonStyle={styles.linkButton}>연동하기</Button> */}
+        <Divider />
+        <Button buttonStyle={styles.cancelButton}>취소하기</Button>
       </View>
     </SafeAreaView>
   );
 };
 
 export default LinkPaymentScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    minHeight: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  content: {
+    marginTop: -30,
+  },
+  title: {
+    color: '#00e787',
+    fontSize: 33,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  subtitle: {
+    color: '#00d17a',
+    fontSize: 26,
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+  typo: {
+    color: '#797979',
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: '600',
+  },
+  linkButton: {
+    backgroundColor: '#00e284',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginVertical: 20,
+    fontWeight: 500,
+  },
+  cancelButton: {
+    backgroundColor: '#e22600',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+    marginTop: 20,
+    fontWeight: 500,
+  },
+});
