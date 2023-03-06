cba91589bc5155d46311fc5f3d77e0a68d0718bf
narinn-star <skfls2618@naver.com>
2023-03-06 19:05:57 +0900

    fix : emailRegex 오류 고침

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -146,7 +146,7 @@ class Utils {
     const regex = /\s/g;
     const removeWhiteSpace = email.replace(regex, '');
     const eRegex =
-      /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+      /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
     console.log('remove : ', eRegex.test(removeWhiteSpace));
     return eRegex.test(removeWhiteSpace);
